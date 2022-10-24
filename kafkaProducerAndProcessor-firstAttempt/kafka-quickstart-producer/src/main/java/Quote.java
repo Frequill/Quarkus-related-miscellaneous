d0147676b3f2316527a25382c25a08366918cd67
Julius Thomsen <89257706+frequill@users.noreply.github.com>
2022-10-24 15:46:25 +0200

    Separated STUPID non-functional code. There are now two separate KAFKA-producer-and-processor directories here. The first one is obsolete, the second one I remade from scratch and managed to get it working.

---
+++ kafkaProducerAndProcessor-firstAttempt/kafka-quickstart-producer/src/main/java/Quote.java
@@ -0,0 +1,25 @@
+public class Quote {
+
+    public String id;
+    public int price;
+
+    /**
+     Default constructor required for Jackson serializer
+     */
+    public Quote(String id, int price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "id='" + id + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+
+
+
+}
